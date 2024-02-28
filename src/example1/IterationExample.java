dbd13520f06dc85c41d6e2079e49929c1a167f65
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-02-28 19:06:44 +0530

    Create IterationExample.java

---
+++ src/example1/IterationExample.java
@@ -0,0 +1,15 @@
+package example1;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class IterationExample {
+    public static void main(String[] args) {
+
+        //immutable list
+        final List<String> friends= List.of("Brian","Nate","Neal","Raju","Sara","Scott");
+
+
+
+    }
+}

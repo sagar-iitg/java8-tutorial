975b259fbaf91fa9157287eed03330dcb64ca948
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-05-12 18:14:46 +0530

    Update LimitExample.java

--- src/example/LimitExample.java
+++ src/example/LimitExample.java
@@ -5,8 +5,10 @@ import java.util.List;
 public  class LimitExample {
 
         public static void main(String[] args) {
-            List<String> names=List.of("shadan","chirag","asif11","arshdeep","harimohan","akshay","pankaj");
+            List<String> names=List.of("shadan","chirag","asif","arshdeep","harimohan","akshay","pankaj");
             names.stream().limit(3).forEach(x->System.out.print(x+" "));
+            System.out.println();
+            names.stream().limit(3).map(String::toUpperCase).forEach(System.out::println);
 
         }
     }

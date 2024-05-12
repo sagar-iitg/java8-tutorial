d4fb1f07dbc6a146e931a4aff280a24270b704c5
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-05-12 20:36:02 +0530

    Create StringCharacter.java

---
+++ src/stringexample/StringCharacter.java
@@ -0,0 +1,9 @@
+package stringexample;
+
+public class StringCharacter {
+    public static void main(String[] args) {
+
+        final String str="ako90djjer0123";
+        str.chars().forEach(System.out::println);
+    }
+}

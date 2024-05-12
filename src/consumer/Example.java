3b279ce695a551178f47f87322daa33880ab57fa
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-05-12 16:24:46 +0530

    Update Example.java

--- src/consumer/Example.java
+++ src/consumer/Example.java
@@ -17,6 +17,24 @@ public class Example {
 
             }
         });
+        System.out.println("---------");
+        //forEach is a higher-order function that accepts a lambda expression
+        // or block of code to execute in the context of each element in the list.
+
+        friendsName.forEach((final String name)-> System.out.println(name));
+        System.out.println("---------");
+        //Java compiler determines the name parameter is a String type,
+        friendsName.forEach((name)-> System.out.println(name));
+
+        //The Java compiler treats single-parameter lambda expressions as special:
+        // we can leave off the parentheses around the parameter if the parameter’s type is inferred.
+        // There’s one caveat: inferred parameters are non-final
+        friendsName.forEach(name-> System.out.println(name));
+
+        // super less code
+        // we used a method reference
+        friendsName.forEach(System.out::println);
+
 
     }
 }

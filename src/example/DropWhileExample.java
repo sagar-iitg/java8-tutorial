eaff9f4490c05558bcfda9dd742404ee3a13d252
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-05-12 18:11:28 +0530

    Update DropWhileExample.java

--- src/example/DropWhileExample.java
+++ src/example/DropWhileExample.java
@@ -5,7 +5,7 @@ import java.util.List;
 public static class DropWhileExample {
 
         public static void main(String[] args) {
-            List<String> names=List.of("shadan","chirag","asif11","arshdeep","harimohan","akshay","pankaj");
+            List<String> names=List.of("shadan","chirag","asif","arshdeep","harimohan","akshay","pankaj");
             names.stream().dropWhile(n->n.length()>4).forEach(x->System.out.print(x+" "));
         }
     }

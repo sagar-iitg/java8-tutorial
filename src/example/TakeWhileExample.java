b7e866985c8c55aa84e0c7c919edf1b57f5d790c
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-05-12 18:17:53 +0530

    Create TakeWhileExample.java

---
+++ src/example/TakeWhileExample.java
@@ -0,0 +1,14 @@
+package example;
+
+import java.util.List;
+
+public  class TakeWhileExample {
+
+        public static void main(String[] args) {
+            List<String> names=List.of("shadan","chirag","asif","arshdeep","harimohan","akshay","pankaj");
+            System.out.println();
+            //like break statement
+            names.stream().takeWhile(name->name.length()>4).map(String::toUpperCase).forEach(System.out::println);
+
+        }
+    }

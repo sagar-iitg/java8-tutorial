0477478446557b063a3851e54d37161146a83896
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-05-12 18:10:43 +0530

    Update Example5.java

--- src/example/Example5.java
+++ src/example/Example5.java
@@ -25,12 +25,5 @@ public class Example5 {
 
     }
 
-    public static class SkipExample {
 
-        public static void main(String[] args) {
-            List<String> names=List.of("shadan","chirag","asif11","arshdeep","harimohan","akshay","pankaj");
-            //names.stream().skip(3).forEach(x->System.out.print(x+" "));
-            names.stream().dropWhile(n->n.length()>4).forEach(x->System.out.print(x+" "));
-        }
-    }
 }

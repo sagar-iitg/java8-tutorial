74b904303d5f8d86db4ea355104d23366c7f9a52
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-08-02 18:06:21 +0530

    Create Main.java

---
+++ src/main/java/streams/q1/Main.java
@@ -0,0 +1,51 @@
+package streams.q1;
+
+
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+class Employee {
+
+
+    int empId;
+    String name;
+    int salary;
+
+}
+class Main{
+
+    public static void main(String[] args) {
+
+        List<Employee> list=new ArrayList<>();
+
+        list.add(new Employee(1, "panka", 100));
+        list.add(new Employee(2, "amit", 1090010));
+        list.add(new Employee(3, "ankit", 100120));
+        list.add(new Employee(4, "mohit", 10000));
+
+        System.out.println(list);
+        String highestSalary = list.stream().
+                max(Comparator.comparing(Employee::getSalary)).
+                map(Employee::getName).orElse("No emp");
+
+        System.out.println(highestSalary);
+
+        Employee highestSalaryEmp = list.stream().
+                max(Comparator.comparing(Employee::getSalary)).orElse(null);
+
+        System.out.println(highestSalaryEmp);
+
+
+    }
+
+}
+
+

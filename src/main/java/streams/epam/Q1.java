8e28234bb0548cbc5f0527d337889ad649ae58be
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-08-29 00:16:55 +0530

    Create Q1.java

---
+++ src/main/java/streams/epam/Q1.java
@@ -0,0 +1,10 @@
+package streams.epam;
+
+import java.util.stream.Stream;
+
+public class Q1 {
+    public static void main(String[] args) {
+        Stream<String> stream1=Stream.of("John","Jane");
+
+    }
+}

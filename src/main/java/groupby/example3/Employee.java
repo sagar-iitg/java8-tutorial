f0eaf9750555d11f484ad92767b9dfbe0cd5317b
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-09-04 12:04:24 +0530

    Create Employee.java

---
+++ src/main/java/groupby/example3/Employee.java
@@ -0,0 +1,24 @@
+package groupby.example3;
+
+class Employee {
+    private String name;
+    private String department;
+
+    public Employee(String name, String department) {
+        this.name = name;
+        this.department = department;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}

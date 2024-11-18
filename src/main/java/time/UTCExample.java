f822eda3c0b1ba653c9e4d57b85fa8ffb57d7631
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-11-18 22:28:53 +0530

    Create UTCExample.java

---
+++ src/main/java/time/UTCExample.java
@@ -0,0 +1,17 @@
+package time;
+
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+
+public class UTCExample {
+    public static void main(String[] args) {
+        // Get the current time in UTC
+        ZonedDateTime utcNow = ZonedDateTime.now(ZoneOffset.UTC);
+        System.out.println("Current UTC Time: " + utcNow);
+        
+        // Alternatively, using Instant to get the current UTC time
+        Instant instant = Instant.now(); // Instant represents a point in time in UTC
+        System.out.println("Current UTC Time (Instant): " + instant);
+    }
+}

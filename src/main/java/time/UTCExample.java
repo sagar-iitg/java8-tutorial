3563b37d88430df6b18a383978d0e4fd2056af90
sk <work.sagarkumar@gmail.com>
2025-01-26 12:10:39 +0530

    added ist time

--- src/main/java/time/UTCExample.java
+++ src/main/java/time/UTCExample.java
@@ -9,6 +9,9 @@ public class UTCExample {
         // Get the current time in UTC
         ZonedDateTime utcNow = ZonedDateTime.now(ZoneOffset.UTC);
         System.out.println("Current UTC Time: " + utcNow);
+
+        ZonedDateTime istNow=ZonedDateTime.now(ZoneOffset.ofHoursMinutes(5,30));
+        System.out.println("ist Time"+istNow);
         
         // Alternatively, using Instant to get the current UTC time
         Instant instant = Instant.now(); // Instant represents a point in time in UTC

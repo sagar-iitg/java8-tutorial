7f6c3e9dc31aa32e806b207c1b10db75cf69c70a
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2025-03-04 01:30:13 +0530

    stream examples

--- src/main/java/conversion/ArrayListToIntArray.java
+++ src/main/java/conversion/ArrayListToIntArray.java
@@ -2,6 +2,8 @@ package conversion;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
 
 public class ArrayListToIntArray {
@@ -21,6 +23,16 @@ public class ArrayListToIntArray {
                 map(row -> row.stream().mapToInt(x -> x).toArray()).
                 toArray(int[][]::new);
         System.out.println(Arrays.deepToString(array));
+
+
+        int[][] arr={{1,2,3},{4,5,6}};
+
+        List<List<String>> list1 = Arrays.stream(arr).
+                map(row -> Arrays.stream(row).
+                        mapToObj(String::valueOf).toList()
+                    ).
+                toList();
+        System.out.println(list1);
 
     }
 }
